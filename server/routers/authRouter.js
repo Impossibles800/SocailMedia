eb5dc4d109ab78d8925e1e8822b94b6d1ef3c048
Abhiraj <195514@nith.ac.in>
2023-02-25 18:21:58 +0530

    Logout Controller

--- server/routers/authRouter.js
+++ server/routers/authRouter.js
@@ -1,10 +1,11 @@
 const router = require('express').Router();
-const {signupController, loginController, RefreshAccessTokenController} = require('../controllers/authController');
+const {signupController, loginController, logoutController, RefreshAccessTokenController} = require('../controllers/authController');
 
 
 router.post('/signup', signupController);
 router.post('/login', loginController);
 router.get('/refresh', RefreshAccessTokenController);
+router.get('/logout', logoutController);
 
 module.exports = router;
 
